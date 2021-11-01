27d77bb4befd13aeced1491f4909c4d6156dd106
James Sherry <james.m.sherry@googlemail.com>
2021-11-01 19:29:56 +0000

    logging2

--- functions/todos-update.js
+++ functions/todos-update.js
@@ -2,12 +2,13 @@ const faunadb = require("faunadb");
 const getId = require("./utils/getId");
 const q = faunadb.query;
 
+console.log(
+  "process.env.FAUNADB_SERVER_SECRET",
+  process.env.FAUNADB_SERVER_SECRET
+);
+
 exports.handler = (event, context) => {
   /* configure faunaDB Client with our secret */
-  console.log(
-    "process.env.FAUNADB_SERVER_SECRET",
-    process.env.FAUNADB_SERVER_SECRET
-  );
   const client = new faunadb.Client({
     secret: process.env.FAUNADB_SERVER_SECRET,
   });
